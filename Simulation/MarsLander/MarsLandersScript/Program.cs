0f0a267c51aac355f4b1287d7843f4ce2b24de72
jardons <sebastien.jardon@moodys.com>
2021-11-01 10:45:41 +0100

    Simulations - Mars Landers - Basic Vertical power Simulation

--- Simulation/MarsLander/MarsLandersScript/Program.cs
+++ Simulation/MarsLander/MarsLandersScript/Program.cs
@@ -21,39 +21,24 @@ namespace MarsLandersScript
 
 	public class Simulator
 	{
-		public const double GRAVITY = 3.711; // m/s²
+		public const double GRAVITY = -3.711; // m/s²
 
 		public State ApplyCommand(Command c, State s)
 		{
-			if (c.Power == 0)
-			{
-				// ^X = Vi * ^t + 1/2 * a * ^t²
-				double deltaY = s.YSpeed + (GRAVITY * (1.0)) / 2.0;
+			double newSpeed = GRAVITY + c.Power;
+			// ^X = Vi * ^t + 1/2 * a * ^t²
+			double deltaY = s.YSpeed + newSpeed / 2.0;
 
-				return new State
-				{
-					Fuel = s.Fuel,
-					Y = s.Y - deltaY,
-					X = s.X,
-					XSpeed = s.XSpeed,
-					YSpeed = s.YSpeed + GRAVITY,
-					Rotate = c.Rotate,
-					Power = c.Power
-				};
-			}
-			else if (c.Power == s.Power)
-				return new State
-				{
-					Fuel = s.Fuel - 1,
-					Y = s.Y,
-					X = s.X,
-					XSpeed = s.XSpeed,
-					YSpeed = s.YSpeed,
-					Rotate = c.Rotate,
-					Power = c.Power
-				};
-			else
-				throw new NotImplementedException();
+			return new State
+			{
+				Fuel = s.Fuel - c.Power,
+				Y = s.Y + deltaY,
+				X = s.X,
+				XSpeed = s.XSpeed,
+				YSpeed = s.YSpeed + newSpeed,
+				Rotate = c.Rotate,
+				Power = c.Power
+			};
 		}
 	}
 
